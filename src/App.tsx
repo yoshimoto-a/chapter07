14f7f962f4a22ca6dbfb0ff18c6903a9e1a664bc
rs-akane <rs.akane.y@gmail.com>
2024-03-03 15:42:37 +0900

    新規コミット

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SiteHeader } from "./SiteHeader";
+import { BlogItem } from "./BlogItem";
+import { Post } from "./Post";
+import { Contact } from "./Contact";
+
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <SiteHeader />
+      <Routes>
+        <Route path="/" element={<BlogItem />} />
+        <Route path="/post/:id" element={<Post />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
